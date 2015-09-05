d664994cef3506eb690dad8e9d0840b68ef3f4a6
Ravalin <rafcio.m@gmail.com>
2015-09-05 15:38:25 +0200

    fixed bug when after closing window app was still working user can now perform single fitting for two and four column files added windows to open and save file

--- src/GUI.py
+++ src/GUI.py
@@ -7,24 +7,33 @@ import fit_old as fo
 import nanograin_size as ns
 import load_files
 import __builtin__
-
+from tkFileDialog import askopenfilename
+from tkFileDialog import asksaveasfilename
+#import os
 
 default_min_size = "1"
 default_max_size = "10"
 global first_mapping
 first_mapping = 1
 arguments_for_map = []
-
+#app_path = os.path.dirname(sys.argv[0])
 ###############################################################################
 #########################DEFINITIONS###########################################
+def on_closing():
+    matplotlib.pyplot.close("all")
+    root.quit()
+
+
 def map_maker():
     # LOADING DATA AND AGGREGATION
+    save_btn.place_forget()
     data = load_files.load_mapping_file(file_name.get())
     aggregated_data = agg.agg_data(data, float(min_size.get()), float(max_size.get()), float(size_step.get()),
                                    float(omega_0.get()), float(gamma_0.get()), float(amplitude.get()),
                                    float(offset.get()))
     data_array = np.array(aggregated_data)
     global f
+    f.clf()
     a = f.add_subplot(1, 1, 1)
     min_value = min(__builtin__.map(min, data_array))
     max_value = max(__builtin__.map(max, data_array))
@@ -38,12 +47,13 @@ def map_maker():
     arguments_for_map.append(data_array)
     arguments_for_map.append(min_value)
     arguments_for_map.append(max_value)
+
     # MAP MAKING
     b = a.pcolor(data_array, cmap='PuBuGn', vmin=min_value,
                  vmax=max_value)  #inne fajne cmap'y do wyboru: 'seismic' , 'coolwarm'
     f.colorbar(b)
     global canvas
-    canvas = FigureCanvasTkAgg(f, master=GUI)
+    canvas = FigureCanvasTkAgg(f, master=root)
     canvas.get_tk_widget().place(x=0, y=30)
     canvas.get_tk_widget().config(border=0)
     notification_label.config(text="")
@@ -58,11 +68,17 @@ def map_maker():
     map_param_rbtn_four.place(x=310, y=5)
     global first_mapping
     first_mapping = 0
+    save_btn.place(x=545, y=1)
 
 
-def single_fitting():
+def single_fitting(are_four_col):
     # LOADING DATA AND AGGREGATION
-    data = load_files.load_mapping_file(file_name.get())
+    save_btn.place_forget()
+    data = None
+    if are_four_col :
+        data = load_files.load_mapping_file(file_name.get())
+    else:
+        data = load_files.load_two_column_file(file_name.get())
     omega = data[2]
     intensity = data[3]
     result = ns.find_grain_diameter(omega, intensity, float(min_size.get()), float(max_size.get()),
@@ -72,9 +88,9 @@ def single_fitting():
     global f
     f = result[1]
     global canvas
-    canvas = FigureCanvasTkAgg(f, master=GUI)
+    canvas = FigureCanvasTkAgg(f, master=root)
     canvas.get_tk_widget().place(x=0, y=30)
-    canvas.get_tk_widget().configure(border=0)
+    canvas.get_tk_widget().configure(border=0, width=650, height=450)
 
     map_rbtn_one.place_forget()
     map_rbtn_two.place_forget()
@@ -85,10 +101,12 @@ def single_fitting():
     map_param_rbtn_two.place_forget()
     map_param_rbtn_three.place_forget()
     map_param_rbtn_four.place_forget()
+    save_btn.place(x=545, y=1)
 
 
 def calibrate():
     # LOADING DATA AND AGGREGATION
+    save_btn.place_forget()
     data = load_files.load_two_column_file(file_name.get())
     omega = data[2]
     intensity = data[3]
@@ -106,10 +124,10 @@ def calibrate():
     global f
     f = result[1]
     global canvas
-    canvas = FigureCanvasTkAgg(f, master=GUI)
+    canvas = FigureCanvasTkAgg(f, master=root)
     canvas.get_tk_widget().place(x=0, y=30)
-    canvas.get_tk_widget().configure(border=0
-                                     )
+    canvas.get_tk_widget().configure(border=0, width=650, height=450)
+
     map_rbtn_one.place_forget()
     map_rbtn_two.place_forget()
     map_rbtn_three.place_forget()
@@ -119,35 +137,65 @@ def calibrate():
     map_param_rbtn_two.place_forget()
     map_param_rbtn_three.place_forget()
     map_param_rbtn_four.place_forget()
+    save_btn.place(x=545, y=1)
 ###############################################################################
-########################GUI ELEMENTS###########################################   
-GUI = tk.Tk()
+########################root ELEMENTS###########################################   
+root = tk.Tk()
 
-GUI.geometry('900x505')
-GUI.title("RamanMaps")
-
+root.geometry('900x505')
+root.title("RamanMaps")
+global f
 f = Figure(facecolor='white')
-canvas = FigureCanvasTkAgg(f, master=GUI)
+global canvas
+canvas = FigureCanvasTkAgg(f, master=root)
 canvas.get_tk_widget().place(x=0, y=30)
 canvas.get_tk_widget().configure(border=0, width=650, height=450)
 notification_label = tk.Label()
 notification_label.config(
-    text="Please note, that mapping may take even a few hours, depending on number of measurements.")
+    text="Please note, that mapping may take several minutes, depending on number of measurements.")
 notification_label.place(x=10, y=484)
 
-file_name_label = tk.Label(text='File name:', font="Helvetica 10 bold")
-file_name_label.place(x=680, y=10)
+# file_name_label = tk.Label(text='File name:', font="Helvetica 10 bold")
+# file_name_label.place(x=680, y=10)
 file_name = tk.Entry()
-file_name.place(x=680, y=30)
-file_name.insert(0, "map_file.txt")
-# file_name.insert(0, "map_file.txt")
+file_name.place(x=680, y=35)
+file_name.config(width=26)
 
 map_btn = tk.Button(text='Create map', command=map_maker, width=18)
 map_btn.place(x=680, y=450)
 
+def save_figure():
+    file_opt = options = {}
+    options['defaultextension'] = '.png'
+    options['filetypes'] = [('vector graphics', '.png')]
+    options['title'] = 'Save image'
+    global f
+    Tk().withdraw() # we don't want a full root, so keep the root window from appearing
+    filepath = asksaveasfilename(**file_opt) # show an "Save" dialog box and return the path to the selected file
+    f.savefig(filepath, bbox_inches='tight')
+
+
+def open_file():
+    file_opt = options = {}
+    options['defaultextension'] = '.txt'
+    options['filetypes'] = [('text files', '.txt')]
+    options['title'] = 'Open text file with data'
+    Tk().withdraw()
+    filepath = askopenfilename(**file_opt)
+    file_name.delete(0, END)
+    file_name.insert(0, filepath)
+    file_name.xview_moveto(1.0)
+
+
+save_btn = tk.Button(text='Save image', command=save_figure, width=10)
+save_btn.place(x=546, y=1)
+save_btn.place_forget()
+
+open_btn = tk.Button(text='Open file', command=open_file, width=6)
+open_btn.place(x=680, y=5)
 
 def radio_button_select(event):
-    if event.widget["value"] == 1 :
+    if event.widget["value"] == 1:
         map_btn.config(text="Create map!", command=map_maker)
 
         max_size_label.config(text="Max grain size to be checked [nm]:")
@@ -168,7 +216,7 @@ def radio_button_select(event):
             notification_label.config(
                 text="Please note, that mapping may take even a few hours, depending on number of measurements.")
     elif event.widget["value"] == 2:
-        map_btn.config(text="Perform fitting!", command=single_fitting)
+        map_btn.config(text="Perform fitting!", command=lambda: single_fitting(True))
         max_size_label.config(text="Max grain size to be checked [nm]:")
         max_size.delete(0, END)
         max_size.insert(0, 10)
@@ -186,6 +234,24 @@ def radio_button_select(event):
         if first_mapping==1:
             notification_label.config(text="")
     elif event.widget["value"] == 3:
+        map_btn.config(text="Perform fitting!", command=lambda: single_fitting(False))
+        max_size_label.config(text="Max grain size to be checked [nm]:")
+        max_size.delete(0, END)
+        max_size.insert(0, 10)
+        min_size.config(text="Min grain size to be checked [nm]:")
+        min_size.delete(0, END)
+        min_size.insert(0, 1)
+
+        max_size.config(state=NORMAL)
+        min_size.config(state=NORMAL)
+        size_step.config(state=NORMAL)
+        omega_0.config(state=NORMAL)
+        gamma_0.config(state=NORMAL)
+        amplitude.config(state=NORMAL)
+        offset.config(state=NORMAL)
+        if first_mapping==1:
+            notification_label.config(text="")
+    elif event.widget["value"] == 4:
         map_btn.config(text="Calibrate!", command=calibrate)
 
         max_size_label.config(text="Max Raman shift")
@@ -205,15 +271,15 @@ def radio_button_select(event):
 
 
 def map_radio_button_select(event):
+    global canvas
     if event.widget["value"] == 1:
         f.clf()
         a = f.add_subplot(1, 1, 1)
         b = a.pcolor(arguments_for_map[0], cmap='Reds', vmin=arguments_for_map[1],
                  vmax=arguments_for_map[2])  #inne fajne cmap'y do wyboru: 'seismic' , 'coolwarm'
         f.colorbar(b)
-        global canvas
         canvas.get_tk_widget().destroy()
-        canvas = FigureCanvasTkAgg(f, master=GUI)
+        canvas = FigureCanvasTkAgg(f, master=root)
         canvas.get_tk_widget().place(x=0, y=30)
         canvas.get_tk_widget().config(border=0, bg='yellow', width=650, height=450)
     if event.widget["value"] == 2:
@@ -222,9 +288,8 @@ def map_radio_button_select(event):
         b = a.pcolor(arguments_for_map[0], cmap='YlOrRd', vmin=arguments_for_map[1],
                  vmax=arguments_for_map[2])  #inne fajne cmap'y do wyboru: 'seismic' , 'coolwarm'
         f.colorbar(b)
-        global canvas
         canvas.get_tk_widget().destroy()
-        canvas = FigureCanvasTkAgg(f, master=GUI)
+        canvas = FigureCanvasTkAgg(f, master=root)
         canvas.get_tk_widget().place(x=0, y=30)
         canvas.get_tk_widget().config(border=0, bg='red', width=650, height=450)
     if event.widget["value"] == 3:
@@ -233,9 +298,8 @@ def map_radio_button_select(event):
         b = a.pcolor(arguments_for_map[0], cmap='winter', vmin=arguments_for_map[1],
                  vmax=arguments_for_map[2])  #inne fajne cmap'y do wyboru: 'seismic' , 'coolwarm'
         f.colorbar(b)
-        global canvas
         canvas.get_tk_widget().destroy()
-        canvas = FigureCanvasTkAgg(f, master=GUI)
+        canvas = FigureCanvasTkAgg(f, master=root)
         canvas.get_tk_widget().place(x=0, y=30)
         canvas.get_tk_widget().config(border=0, bg='black', width=650, height=450)
     if event.widget["value"] == 4:
@@ -244,23 +308,22 @@ def map_radio_button_select(event):
         b = a.pcolor(arguments_for_map[0], cmap='rainbow', vmin=arguments_for_map[1],
                  vmax=arguments_for_map[2])  #inne fajne cmap'y do wyboru: 'seismic' , 'coolwarm'
         f.colorbar(b)
-        global canvas
         canvas.get_tk_widget().destroy()
-        canvas = FigureCanvasTkAgg(f, master=GUI)
+        canvas = FigureCanvasTkAgg(f, master=root)
         canvas.get_tk_widget().place(x=0, y=30)
         canvas.get_tk_widget().config(border=0, bg='green', width=650, height=450)
 
 
 def map_param_radio_button_select(event):
+    global canvas
     if event.widget["value"] == 1:
         f.clf()
         a = f.add_subplot(1, 1, 1)
         b = a.pcolor(arguments_for_map[0], cmap='PuBuGn', vmin=arguments_for_map[1],
                  vmax=arguments_for_map[2])  #inne fajne cmap'y do wyboru: 'seismic' , 'coolwarm'
         f.colorbar(b)
-        global canvas
         canvas.get_tk_widget().destroy()
-        canvas = FigureCanvasTkAgg(f, master=GUI)
+        canvas = FigureCanvasTkAgg(f, master=root)
         canvas.get_tk_widget().place(x=0, y=30)
         canvas.get_tk_widget().config(border=0, bg='yellow', width=650, height=450)
     if event.widget["value"] == 2:
@@ -269,9 +332,8 @@ def map_param_radio_button_select(event):
         b = a.pcolor(arguments_for_map[0], cmap='coolwarm', vmin=arguments_for_map[1],
                  vmax=arguments_for_map[2])  #inne fajne cmap'y do wyboru: 'seismic' , 'coolwarm'
         f.colorbar(b)
-        global canvas
         canvas.get_tk_widget().destroy()
-        canvas = FigureCanvasTkAgg(f, master=GUI)
+        canvas = FigureCanvasTkAgg(f, master=root)
         canvas.get_tk_widget().place(x=0, y=30)
         canvas.get_tk_widget().config(border=0, bg='red', width=650, height=450)
     if event.widget["value"] == 3:
@@ -280,9 +342,8 @@ def map_param_radio_button_select(event):
         b = a.pcolor(arguments_for_map[0], cmap='seismic', vmin=arguments_for_map[1],
                  vmax=arguments_for_map[2])  #inne fajne cmap'y do wyboru: 'seismic' , 'coolwarm'
         f.colorbar(b)
-        global canvas
         canvas.get_tk_widget().destroy()
-        canvas = FigureCanvasTkAgg(f, master=GUI)
+        canvas = FigureCanvasTkAgg(f, master=root)
         canvas.get_tk_widget().place(x=0, y=30)
         canvas.get_tk_widget().config(border=0, bg='black', width=650, height=450)
     if event.widget["value"] == 4:
@@ -291,37 +352,44 @@ def map_param_radio_button_select(event):
         b = a.pcolor(arguments_for_map[0], cmap='rainbow', vmin=arguments_for_map[1],
                  vmax=arguments_for_map[2])  #inne fajne cmap'y do wyboru: 'seismic' , 'coolwarm'
         f.colorbar(b)
-        global canvas
         canvas.get_tk_widget().destroy()
-        canvas = FigureCanvasTkAgg(f, master=GUI)
+        canvas = FigureCanvasTkAgg(f, master=root)
         canvas.get_tk_widget().place(x=0, y=30)
         canvas.get_tk_widget().config(border=0, bg='green', width=650, height=450)
 
 
 
 v = tk.IntVar()
-rbtn_one = tk.Radiobutton(GUI,
+rbtn_one = tk.Radiobutton(root,
                           text="Mapping file (four columns)",
                           padx=0,
                           variable=v,
                           value=1)
-rbtn_one.place(x=670, y=60)
+rbtn_one.place(x=670, y=53)
 rbtn_one.select()
 rbtn_one.bind("<Button-1>", radio_button_select)
-rbtn_two = tk.Radiobutton(GUI,
-                          text="Single sample file (two columns)",
+rbtn_two = tk.Radiobutton(root,
+                          text="Single sample file (four columns)",
                           padx=0,
                           variable=v,
                           value=2)
-rbtn_two.place(x=670, y=80)
+rbtn_two.place(x=670, y=73)
 rbtn_two.bind("<Button-1>", radio_button_select)
-rbtn_three = tk.Radiobutton(GUI,
+rbtn_three = tk.Radiobutton(root,
+                          text="Single sample file (two columns)",
+                          padx=0,
+                          variable=v,
+                          value=3)
+rbtn_three.place(x=670, y=93)
+rbtn_three.bind("<Button-1>", radio_button_select)
+rbtn_four = tk.Radiobutton(root,
                             text="Calibration file (two columns)",
                             padx=0,
                             variable=v,
-                            value=3)
-rbtn_three.place(x=670, y=100)
-rbtn_three.bind("<Button-1>", radio_button_select)
+                            value=4)
+rbtn_four.place(x=670, y=113)
+rbtn_four.bind("<Button-1>", radio_button_select)
+
 
 
 # initial_shift_label = tk.Label(text='Initial Raman Shift [cm^-1]:')
@@ -337,52 +405,52 @@ rbtn_three.bind("<Button-1>", radio_button_select)
 #final_shift.insert(0, "600")
 
 min_size_label = tk.Label(text="Min grain size to be checked [nm]:")
-min_size_label.place(x=680, y=130)
+min_size_label.place(x=680, y=140)
 min_size = tk.Entry()
-min_size.place(x=680, y=145)
+min_size.place(x=680, y=155)
 min_size.insert(0, 1)
 
 max_size_label = tk.Label(text="Max grain size to be checked [nm]:")
-max_size_label.place(x=680, y=170)
+max_size_label.place(x=680, y=180)
 max_size = tk.Entry()
-max_size.place(x=680, y=185)
+max_size.place(x=680, y=195)
 max_size.insert(0, 10)
 
 size_step_label = tk.Label(text="Step when checking size [nm]:")
-size_step_label.place(x=680, y=210)
+size_step_label.place(x=680, y=220)
 size_step = tk.Entry()
-size_step.place(x=680, y=225)
+size_step.place(x=680, y=235)
 size_step.insert(0, 0.5)
 
 parameters_label = tk.Label(text="Parameters of main peak:", font="Helvetica 10 bold")
-parameters_label.place(x=680, y=255)
+parameters_label.place(x=680, y=265)
 
 omega_0_label = tk.Label(text="omega [cm^-1]")
-omega_0_label.place(x=680, y=275)
+omega_0_label.place(x=680, y=285)
 omega_0 = tk.Entry()
 omega_0.place(x=680, y=290)
 omega_0.insert(0, "521.604599812")
 
 gamma_0_label = tk.Label(text="gamma [cm^-1]")
-gamma_0_label.place(x=680, y=315)
+gamma_0_label.place(x=680, y=325)
 gamma_0 = tk.Entry()
-gamma_0.place(x=680, y=335)
+gamma_0.place(x=680, y=345)
 gamma_0.insert(0, "4.42010161931")
 
 amplitude_label = tk.Label(text="ampl [arb. u]")
-amplitude_label.place(x=680, y=360)
+amplitude_label.place(x=680, y=370)
 amplitude = tk.Entry()
-amplitude.place(x=680, y=375)
+amplitude.place(x=680, y=385)
 amplitude.insert(0, "71535.2586963")
 
 offset_label = tk.Label(text="offset [arb. u]")
-offset_label.place(x=680, y=400)
+offset_label.place(x=680, y=410)
 offset = tk.Entry()
-offset.place(x=680, y=415)
+offset.place(x=680, y=425)
 offset.insert(0, "0.0")
 
 map_var = tk.IntVar()
-map_rbtn_one = tk.Radiobutton(GUI,
+map_rbtn_one = tk.Radiobutton(root,
                           text="mode1",
                           padx=0,
                           variable=map_var,
@@ -390,19 +458,19 @@ map_rbtn_one = tk.Radiobutton(GUI,
 map_rbtn_one.select()
 map_rbtn_one.bind("<Button-1>", map_radio_button_select)
 
-map_rbtn_two = tk.Radiobutton(GUI,
+map_rbtn_two = tk.Radiobutton(root,
                           text="mode2",
                           padx=0,
                           variable=map_var,
                           value=2)
 map_rbtn_two.bind("<Button-1>", map_radio_button_select)
-map_rbtn_three = tk.Radiobutton(GUI,
+map_rbtn_three = tk.Radiobutton(root,
                             text="mode3",
                             padx=0,
                             variable=map_var,
                             value=3)
 map_rbtn_three.bind("<Button-1>", map_radio_button_select)
-map_rbtn_four = tk.Radiobutton(GUI,
+map_rbtn_four = tk.Radiobutton(root,
                             text="mode4",
                             padx=0,
                             variable=map_var,
@@ -414,7 +482,7 @@ map_rbtn_three.place_forget()
 map_rbtn_four.place_forget()
 
 map_param_var = tk.IntVar()
-map_param_rbtn_one = tk.Radiobutton(GUI,
+map_param_rbtn_one = tk.Radiobutton(root,
                           text="diameter",
                           padx=0,
                           variable=map_param_var,
@@ -422,19 +490,19 @@ map_param_rbtn_one = tk.Radiobutton(GUI,
 map_param_rbtn_one.select()
 map_param_rbtn_one.bind("<Button-1>", map_param_radio_button_select)
 
-map_param_rbtn_two = tk.Radiobutton(GUI,
+map_param_rbtn_two = tk.Radiobutton(root,
                           text="mode2",
                           padx=0,
                           variable=map_param_var,
                           value=2)
 map_param_rbtn_two.bind("<Button-1>", map_param_radio_button_select)
-map_param_rbtn_three = tk.Radiobutton(GUI,
+map_param_rbtn_three = tk.Radiobutton(root,
                             text="mode3",
                             padx=0,
                             variable=map_param_var,
                             value=3)
 map_param_rbtn_three.bind("<Button-1>", map_param_radio_button_select)
-map_param_rbtn_four = tk.Radiobutton(GUI,
+map_param_rbtn_four = tk.Radiobutton(root,
                             text="mode4",
                             padx=0,
                             variable=map_param_var,
@@ -445,6 +513,7 @@ map_param_rbtn_two.place_forget()
 map_param_rbtn_three.place_forget()
 map_param_rbtn_four.place_forget()
 
-GUI.mainloop()
+root.protocol("WM_DELETE_WINDOW", on_closing)
+root.mainloop()
 
 
