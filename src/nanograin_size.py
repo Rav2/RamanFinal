8978edef289b4a3b8ec29d7c4287e3708b692316
wojbry <brylinski.wojciech@gmail.com>
2016-01-02 18:08:37 +0100

    Lmfit implemented for fitting Phonon Confinement function (for now works only for main peak - better for data from 7-09-2015)

--- src/nanograin_size.py
+++ src/nanograin_size.py
@@ -6,7 +6,7 @@ import fit_lorentz
 import load_files
 import fit_old
 from lmfit import Model, CompositeModel
-from lmfit.models import  LorentzianModel, LinearModel
+from lmfit.models import LorentzianModel, LinearModel
 from lmfit.lineshapes import lorentzian
 #########################################################################
 ############################# INITIAL VALUES FROM FILE "fit_double_lorentz.py" ##############################
@@ -51,13 +51,16 @@ def total_intensity(omega, d, N, omega_0, gamma_0):
     return h * inten
 
 
+def total_intensity_fit_func(x, d, N, omega_0, gamma_0, amplitude):
+    return amplitude*normalized_intensity(x, d, N, omega_0, gamma_0)
+
 def find_max_intensity(d, omega_0, gamma_0):
     omega1 = arange(500, 530, 0.01)
     return max(total_intensity(omega1, d, 100, omega_0, gamma_0))
 
 
-def normalized_intensity(omega, d, N, omega_0, gamma_0):
-    return total_intensity(omega, d, N, omega_0, gamma_0) / find_max_intensity(d, omega_0, gamma_0)
+def normalized_intensity(x, d, N, omega_0, gamma_0):
+    return total_intensity(x, d, N, omega_0, gamma_0) / find_max_intensity(d, omega_0, gamma_0)
 
 
 def find_grain_diameter(X, Y, min_size, max_size, size_step, omega_0, gamma_0, peak_height, return_figure, sample_type,  peaks_params):
@@ -77,103 +80,122 @@ def find_grain_diameter(X, Y, min_size, max_size, size_step, omega_0, gamma_0, p
     :return:
     """
     all_lorentz = True
-    for i in range(0, len(peaks_params)):
-        all_lorentz *= peaks_params[i][6]
+    #for i in range(0, len(peaks_params)):
 
-        X_ex = X #dlaczego
-        Y_ex = fit_lorentz.perform_fitting(X_ex, Y, peaks_params)
+        #all_lorentz *= peaks_params[i][6]
 
-        fitting_result = fit_old.perform_fitting(X_ex, Y_ex, 500, 580)
-        single_lorentz_parameters = fitting_result[0]
-        #print single_lorentz_parameters
-        x_constraint = (X_ex < 535) * (X_ex > 510)
-        X_fit = X_ex[x_constraint]
-        Y_fit = Y_ex[x_constraint]
+    X_ex = X #dlaczego
+    Y_ex = fit_lorentz.perform_fitting(X_ex, Y, peaks_params)
 
-        list = arange(min_size, max_size, size_step)  # lista grubosci nanoziaren
-        suma = []
-        omega_exp = arange(450, 600, 1)
-        for diameter in list:
-            delta = 0
-            delta = np.sum(np.abs(Y_fit - (single_lorentz_parameters[2] * normalized_intensity(X_fit, diameter, 100, omega_0, gamma_0) + single_lorentz_parameters[3])))
-            suma.append(delta)
+    fitting_result = fit_old.perform_fitting(X_ex, Y_ex, 500, 580)
+    single_lorentz_parameters = fitting_result[0]
+    #print single_lorentz_parameters
+    x_constraint = (X_ex < 535) * (X_ex > 510)
+    X_fit = X_ex[x_constraint]
+    Y_fit = Y_ex[x_constraint]
 
-        print (min(suma), suma.index(min(suma)) * size_step + min_size)
-        nanograin_size = suma.index(min(suma)) * size_step + min_size
+    list = arange(min_size, max_size, size_step)  # lista grubosci nanoziaren
+    suma = []
+    omega_exp = arange(450, 600, 1)
+    for diameter in list:
+        delta = 0
+        delta = np.sum(np.abs(Y_fit - (single_lorentz_parameters[2] * normalized_intensity(X_fit, diameter, 100, omega_0, gamma_0) + single_lorentz_parameters[3])))
+        suma.append(delta)
 
-        if all_lorentz:
+    print (min(suma), suma.index(min(suma)) * size_step + min_size)
+    nanograin_size = suma.index(min(suma)) * size_step + min_size
+
+
         ###mozna przerobic program tak, zeby sprawdzal czy dany pik nie jest lorentzowski i wtedy dodawal instancje Model
         ### utworzona w oparciu o nasza funkcje do szukania rozmiaru ziaren
-            max_y = max(Y)
-            lin_mod = LinearModel(prefix="background_", min=0, max=0.5*max_y)
-            pars = lin_mod.guess(Y, x=X)
-            final_mod = lin_mod
-            for ii in range(0,len(peaks_params)):
-                mod = LorentzianModel(prefix="l" + str(ii) + '_')
-                pars.update(mod.make_params())
-                pars['l'+str(ii)+'_center'].set(peaks_params[ii][0], min=peaks_params[ii][3], max=peaks_params[ii][4])
-                ###TODO: sigma ponizej jest tozsama z gamma podanym przez uzytkownika, a powinno byc inaczej (patrz dokumentacja lmfit)!!! Uzytkownik powinien podawac par. sigma. (Dla poprawnosci dopasowania to wszystko jedno ale dla zrozumienia wynikow nie.)
-                pars['l'+str(ii)+'_sigma'].set(peaks_params[ii][1])
-                pars['l'+str(ii)+'_amplitude'].set(peaks_params[ii][2], min=0)#, max=max_y)
-                final_mod = final_mod + mod
+    max_y = max(Y)
+    lin_mod = LinearModel(prefix="background_", min=0, max=0.5*max_y)
+    pars = lin_mod.guess(Y, x=X)
+    final_mod = lin_mod
+    for ii in range(0,len(peaks_params)):
+        if peaks_params[ii][6]==True:
+            mod = LorentzianModel(prefix="l" + str(ii) + '_')
+            pars.update(mod.make_params())
+            pars['l'+str(ii)+'_center'].set(peaks_params[ii][0], min=peaks_params[ii][3], max=peaks_params[ii][4])
+            ###TODO: sigma ponizej jest tozsama z gamma podanym przez uzytkownika, a powinno byc inaczej (patrz dokumentacja lmfit)!!! Uzytkownik powinien podawac par. sigma. (Dla poprawnosci dopasowania to wszystko jedno ale dla zrozumienia wynikow nie.)
+            pars['l'+str(ii)+'_sigma'].set(peaks_params[ii][1])
+            pars['l'+str(ii)+'_amplitude'].set(peaks_params[ii][2], min=0)#, max=max_y)
+        else:
+            mod = Model(total_intensity_fit_func)#total_intensity_fit_func(omega, d, N, omega_0, gamma_0, amplitude)
+            pars.update(mod.make_params())
+            pars['d'].set(10, min=0, max=30)
+            pars['N'].set(100, vary=False)
+            pars['omega_0'].set(peaks_params[ii][0], vary=False)
+            pars['gamma_0'].set(peaks_params[ii][1], vary=False)
+            pars['amplitude'].set(0.8*max_y, min=0, max=max_y)
+        final_mod = final_mod + mod
                 # print("adding a peak!")
             ####PLOT###
-            my_figure = plt.Figure()
-            x_range = (X>400)*(X<600)
-            my_plot = my_figure.add_subplot(111)
-            legend_labels = []
-            for ax in my_figure.axes:
-                ax.set_xlabel(u'\u03C9'+' [cm^-1]')
-                ax.set_ylabel('Intensity [arb.]')
+    my_figure = plt.Figure()
+    x_range = (X>400)*(X<600)
+    my_plot = my_figure.add_subplot(111)
+    legend_labels = []
+    for ax in my_figure.axes:
+        ax.set_xlabel(u'\u03C9'+' [cm^-1]')
+        ax.set_ylabel('Intensity [arb.]')
 
-            my_plot.plot(X[x_range], Y[x_range], "ro")
-            legend_labels.append('Data')
+    my_plot.plot(X[x_range], Y[x_range], "ro")
+    legend_labels.append('Data')
             # init = final_mod.eval(pars, x=X)
             # my_plot.plot(X[x_range], init[x_range], 'k--')
 
-            out = final_mod.fit(Y, pars, x=X)
-            if not out.success:
-                print("!!!!!!!!!!!!!FIT HAVE FAILED!!!!!!!!!!!")
-                legend_labels = ['FIT FAILED!']
-                my_plot.legend(legend_labels)
-                return nanograin_size, single_lorentz_parameters[0], abs(single_lorentz_parameters[1]), single_lorentz_parameters[2], my_figure
+    out = final_mod.fit(Y, pars, x=X)
+    if not out.success:
+        print("!!!!!!!!!!!!!FIT HAVE FAILED!!!!!!!!!!!")
+        legend_labels = ['FIT FAILED!']
+        my_plot.legend(legend_labels)
+        return nanograin_size, single_lorentz_parameters[0], abs(single_lorentz_parameters[1]), single_lorentz_parameters[2], my_figure
 
-            else:
-                print(out.fit_report(min_correl=0.5))
-                legend_labels.append('Best fit')
+    else:
+        print(out.fit_report(min_correl=0.5))
+        legend_labels.append('Best fit')
 
-            my_plot.plot(X[x_range], out.best_fit[x_range], "b-")
+    my_plot.plot(X[x_range], out.best_fit[x_range], "b-")
         
-            my_plot.plot(X[x_range], out.best_values['background_slope']*X[x_range]+out.best_values['background_intercept'], '--')
-            legend_labels.append('background')
-            for jj in range(0, len(peaks_params)):
-                if(peaks_params[jj][5]==True):
-                    sigma = out.best_values['l'+str(jj)+'_sigma']
-                    center = out.best_values['l'+str(jj)+'_center']
-                    amplitude = out.best_values['l'+str(jj)+'_amplitude']
-                    legend_labels.append('l'+str(jj))
-                    my_plot.plot(X[x_range], lorentzian(X[x_range], center=center, sigma=sigma, amplitude=amplitude), '--')
-            my_plot.legend(legend_labels)
-            return nanograin_size, single_lorentz_parameters[0], abs(single_lorentz_parameters[1]), single_lorentz_parameters[2], my_figure
+    my_plot.plot(X[x_range], out.best_values['background_slope']*X[x_range]+out.best_values['background_intercept'], '--')
+    legend_labels.append('background')
+    for jj in range(0, len(peaks_params)):
+        if(peaks_params[jj][5]==True):
+            if peaks_params[jj][6]==True:
+                sigma = out.best_values['l'+str(jj)+'_sigma']
+                center = out.best_values['l'+str(jj)+'_center']
+                amplitude = out.best_values['l'+str(jj)+'_amplitude']
+                legend_labels.append('l'+str(jj))
+                my_plot.plot(X[x_range], lorentzian(X[x_range], center=center, sigma=sigma, amplitude=amplitude), '--')
+            else:
+                sigma = out.best_values['gamma_0']
+                center = out.best_values['omega_0']
+                amplitude = out.best_values['amplitude']
+                d = out.best_values['d']
+                N = out.best_values['N']
+                legend_labels.append('fc'+str(jj))
+                my_plot.plot(X[x_range], total_intensity_fit_func(X[x_range], d, N, center, sigma, amplitude), '--')
+    my_plot.legend(legend_labels)
+    return nanograin_size, single_lorentz_parameters[0], abs(single_lorentz_parameters[1]), single_lorentz_parameters[2], my_figure
 
-        if return_figure:
-            figure = plt.Figure()
-            my_plot = figure.add_subplot(111)
-            my_plot.plot(omega_exp,
+    if return_figure:
+        figure = plt.Figure()
+        my_plot = figure.add_subplot(111)
+        my_plot.plot(omega_exp,
                          [single_lorentz_parameters[2] * normalized_intensity(omega, nanograin_size, 100, omega_0, gamma_0) + single_lorentz_parameters[3]
                           for omega in omega_exp], label="fitted for %snm" % nanograin_size)
-            my_plot.plot(X_fit, Y_fit, ".", label="exp data", color="c")
-            figure.suptitle("d:{0:.1f}[nm]   omega:{1:.4f}[cm^-1]   hwhm:{2:.4f}[cm^-1]   inten:{3:.4}[arb]".format(nanograin_size, single_lorentz_parameters[0], single_lorentz_parameters[1], single_lorentz_parameters[2]))
-            my_plot.legend(loc=1)
-            return nanograin_size, single_lorentz_parameters[0], abs(single_lorentz_parameters[1]), single_lorentz_parameters[2], figure
-        else:
-            return nanograin_size, single_lorentz_parameters[0], abs(single_lorentz_parameters[1]), single_lorentz_parameters[2]
+        my_plot.plot(X_fit, Y_fit, ".", label="exp data", color="c")
+        figure.suptitle("d:{0:.1f}[nm]   omega:{1:.4f}[cm^-1]   hwhm:{2:.4f}[cm^-1]   inten:{3:.4}[arb]".format(nanograin_size, single_lorentz_parameters[0], single_lorentz_parameters[1], single_lorentz_parameters[2]))
+        my_plot.legend(loc=1)
+        return nanograin_size, single_lorentz_parameters[0], abs(single_lorentz_parameters[1]), single_lorentz_parameters[2], figure
+    else:
+        return nanograin_size, single_lorentz_parameters[0], abs(single_lorentz_parameters[1]), single_lorentz_parameters[2]
 
 #########################################################################    
 def main():
     #########################################################################
     ############################# LOADING DATA ##############################   
-    result = load_files.load_mapping_file("../content/SemiAmorphS1/25.5.txt")
+    result = load_files.load_mapping_file("../content/2015-04-10Si-mapySingleFiles/SemiAmorphS1/25.5.txt")
     X_ex = result[2]
     Y_ex = fit_lorentz.perform_fitting(X_ex, result[3], 522, 4, 0, 3)
     d = find_grain_diameter(X_ex, Y_ex, 1., 20., 0.1, 522, 4, 2.13334158e+04, 0, 3)
